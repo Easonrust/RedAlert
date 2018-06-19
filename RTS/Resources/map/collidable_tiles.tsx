1eeda37d278331e6f43b01f756f901f0cafa189c
Easonrust <752471498@qq.com>
2018-06-19 14:59:29 +0800

    #4

--- RTS/Resources/map/collidable_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="collidable_tiles" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="collidable_tiles.jpg" width="32" height="32"/>
-</tileset>
